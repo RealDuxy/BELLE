78bf12c606ee4efe56c76ecc2a2a4fff255b1c90
RealDuxy <paliss001@outlook.com>
2023-04-11 16:48:51 +0800

    添加训练脚本

--- train/run_bloomz_lora.sh
+++ train/run_bloomz_lora.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#SBATCH-gpus=2
+#SBATCH --gpus=2
 
 torchrun --nproc_per_node=2 finetune.py --model_config_file run_config/Bloom_config.json --lora_hyperparams_file run_config/lora_hyperparams_bloom.json --use_lora
+
